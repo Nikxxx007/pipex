b98eb46d1d50e49017795dc16bd0c99455960fd3
Rosalinda Gaynell <rogaynel@il-h3.msk.21-school.ru>
2021-08-13 15:29:36 +0300

    start

--- src/main.c
+++ src/main.c
@@ -1,16 +1,32 @@
 #include "../includes/pipex.h"
+#include <unistd.h>
+#include <stdio.h>
 
-void	prog_exit(char *mes, int value)
+void    prog_exit(char *mes, int value)
 {
 
 }
 
-int	main(int argc, char **argv, char **envp)
+int    main(int argc, char **argv, char **envp)
 {
-	if (argc != 5)
-		prog_exit("Incorrect data", 1);
-	else
-	{
-
-	}
+//    if (argc != 5)
+//        prog_exit("Incorrect data", 1);
+//    else
+//    {
+//
+//    }
+    printf("Before fork\n");
+    pid_t pid = fork();
+    if (pid == 0) {
+        printf("child process %d \n", pid);
+        exit(123);
+    }
+    else
+    {
+        printf("parent process %d\n", pid);
+    }
+    int a = 0;
+    wait(&a);
+    printf("Hunia %d\n", WEXITSTATUS(a));
+    return 0;
 }
